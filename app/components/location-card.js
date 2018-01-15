dfe74f1d9ee90607823b013a2f4f1c057f427fc4
Ryan Jones <ryandjones14@gmail.com>
2018-01-15 17:01:14 -0600

    created wish-list comp, added action to addToWishList to location-card

--- app/components/location-card.js
+++ app/components/location-card.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isInWishList: false,
+  buttonText: Ember.computed('isInWishList', function() {
+    return this.get('isInWishList') ? 'Remove from Wish List' : 'Add to Wish List';
+  }),
+
+  actions: {
+    toggleWishList: function() {
+      this.toggleProperty('isInWishList');
+
+    }
+  }
 });
